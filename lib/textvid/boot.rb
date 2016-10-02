f8199154b1364c4d3c1e77426d9d207429a5cd68
Keita Mizuochi <keitax913@yahoo.co.jp>
2016-10-02 19:23:01 +0900

    boot: Add the boot script

---
+++ lib/textvid/boot.rb
@@ -0,0 +1,14 @@
+require 'textvid/application'
+require 'textvid/database'
+require 'textvid/router'
+
+module Textvid
+  def self.main
+    Application.set(:database, Database.create('textvid-db'))
+    Application.set(:router, Router.new('http://localhost:4567/'))
+    Application.set(:site_title, "Textvid Blog")
+    Application.run!
+  end
+end
+
+Textvid.main
