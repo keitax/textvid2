0c8de0b69a20608d7e303110960ca0133e42847c
Keita Mizuochi <keitax913@yahoo.co.jp>
2016-10-02 18:55:07 +0900

    router: Implement the post_list_url method

--- lib/textvid/router.rb
+++ lib/textvid/router.rb
@@ -13,5 +13,22 @@ module Textvid
       post_path = sprintf("%02d/%02d/%s.html", post.created_at.year, post.created_at.month, post.url_title)
       URI.join(@root_path, URI.encode(post_path))
     end
+
+    def post_list_url(selector)
+      params = []
+      if selector.label
+        params.push("label=#{URI.encode(selector.label)}")
+      end
+      if selector.start
+        params.push("start=#{selector.start}")
+      end
+      if selector.results
+        params.push("results=#{selector.results}")
+      end
+      if params.empty?
+        return URI.join(@root_path, "posts/")
+      end
+      URI.join(@root_path, "posts/", "?#{params.join('&')}")
+    end
   end
 end
--- test/router_test.rb
+++ test/router_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'uri'
 
+require 'textvid/database'
 require 'textvid/entity'
 require 'textvid/router'
 
@@ -22,5 +23,16 @@ class RouterTest < Minitest::Test
 
     assert_equal(URI('http://localhost:8000/root/2016/01/test%20post.html'), @router.post_url(post_with_url_title))
     assert_equal(URI('http://localhost:8000/root/posts/1100'), @router.post_url(post_without_url_title))
+  end
+
+  def test_post_list_url
+    nil_query = Database::Query.new
+    assert_equal(URI('http://localhost:8000/root/posts/'), @router.post_list_url(nil_query))
+
+    specified_query = Database::Query.new
+    specified_query.label = 'text vid'
+    specified_query.start = 1
+    specified_query.results = 2
+    assert_equal(URI('http://localhost:8000/root/posts/?label=text%20vid&start=1&results=2'), @router.post_list_url(specified_query))
   end
 end
