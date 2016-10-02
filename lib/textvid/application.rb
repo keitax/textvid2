7bd889d6dbf5fe1edccb50b5d5156a89a02e0514
Keita Mizuochi <keitax913@yahoo.co.jp>
2016-10-02 19:24:59 +0900

    application: Add the index controller

---
+++ lib/textvid/application.rb
@@ -0,0 +1,32 @@
+require 'sinatra/base'
+require 'erb'
+require 'forwardable'
+
+require 'textvid/database'
+require 'textvid/query'
+
+module Textvid
+  class Application < Sinatra::Base
+    include ERB::Util
+
+    extend Forwardable
+    def_delegator :settings, :database
+    def_delegator :settings, :router
+
+    POSTS_PER_PAGE = 5
+
+    get '/' do
+      @posts = database.select(default_query)
+      erb :index
+    end
+
+    private
+
+    def default_query
+      q = Query.new
+      q.start = 0
+      q.results = POSTS_PER_PAGE
+      q
+    end
+  end
+end
