b3ae755d9cc738e521ab7e2ad2a48e892279f193
Keita Mizuochi <keitax913@yahoo.co.jp>
2016-10-30 02:38:50 +0900

    query: Add #previous and #next methods

--- lib/textvid/query.rb
+++ lib/textvid/query.rb
@@ -1,5 +1,20 @@
 module Textvid
   class Query
     attr_accessor :words, :label, :start, :results, :year, :month, :url_title
+
+    def previous
+      return nil unless @start && @results
+      q = self.dup
+      q.start = [1, @start - @results].max
+      q.results = @start - q.start
+      q
+    end
+
+    def next
+      return nil unless @start && @results
+      q = self.dup
+      q.start += @results
+      q
+    end
   end
 end
